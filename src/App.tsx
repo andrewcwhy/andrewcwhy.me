52b550b01bb7acc14662460813bc308ed707327a
Andrew Young <acydeveloper05@gmail.com>
2025-03-17 17:28:40 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from '@/pages/Home'
-import About from '@/pages/About'
-import Contact from '@/pages/Contact'
-import Nav from '@/components/Nav'
-import Footer from '@/components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
 
 export default function App() {
     return (
